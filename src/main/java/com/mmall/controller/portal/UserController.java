f7789bc5b6bb3ccaf0d9c3e70d19d5425b36758a
TooHardForAName <18392371793@163.com>
2018-11-26 00:34:29 +0800

    check_valid.do AND MD5

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -25,16 +25,54 @@ public class UserController {
      * @param session
      * @return
      */
-    @RequestMapping(value = "login.do",method = RequestMethod.GET)
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session){
         ServerResponse<User> response=iUserService.login(username,password);
+        //判断是否登录成功，if为true则表示登录成功。将当前登录对象放入session中
         if (response.isSuccess()){
             session.setAttribute(Const.CURRENT_USER,response.getData());
         }
-
-//注sdfasdfsadfas
         return response;
     }
+
+    /**
+     * 用户登出
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "logout.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        //在登录状态下退出登录，即将登录对象从session中移除。
+        session.removeAttribute(Const.CURRENT_USER);
+
+        return ServerResponse.createBySuccess();
+    }
+
+    /**
+     * 注册
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "register.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> register(User user){
+
+        return iUserService.register(user);
+    }
+
+    /**
+     * 验证用户名或者邮箱是否已经被注册
+     * @param str 用户名或者邮箱
+     * @param type 表示类型是用户名或者邮箱
+     * @return
+     */
+    @RequestMapping(value = "check_valid.do",method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<String> checkValid(String str,String type){
+        return iUserService.checkValid(str,type);
+    }
+
 
 }
